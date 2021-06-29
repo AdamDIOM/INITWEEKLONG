2b6215fbe668661183d98fdd36c09d0b45427195
Adam Cameron Drummond <66436400+AdamDIOM@users.noreply.github.com>
2021-06-29 15:54:20 +0100

    Add files via upload

---
+++ reminder app for long term goal.py
@@ -0,0 +1,4 @@
+if input("have you completed your course yet? >_") == "yes":
+    print("congratulations!! well done. you've completed an extremely long-term goal!")
+else:
+    print("Adam what are you doing. finish that course now!!!")
